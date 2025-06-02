5cf747c21f7e2af866dfa6cdc7ded337e8ce3161
Ivana Radosevic Miricki <ivana_radosevicmiricki@epam.com>
2025-06-02 21:52:20 +0200

    Home page tests - refactoring and updating comment option

--- cypress/e2e/home/home.spec.js
+++ cypress/e2e/home/home.spec.js
@@ -1,38 +1,55 @@
-const homePage = require('../../pages/homePage');
+const homePage = require("../../pages/homePage");
 
-describe('Home Page tests', () => {
+describe("Home Page", () => {
   beforeEach(() => {
-    cy.session('api-login', () => {
-      cy.loginViaAPI(); // koristi tvoju custom komandu iz commands.js
+    cy.session("api-login", () => {
+      cy.loginViaAPI(); // using custom commande from commands.js
     });
-    cy.visit(Cypress.env('productionHomePage'));
+    cy.visit(Cypress.env("productionHomePage"));
   });
 
-  it('Posts are present on the Home page', () => {
-    homePage.assertPostsArePresent();
-  })
+  describe("Home Page - content", () => {
+    it("Posts are present on the Home page", () => {
+      homePage.assertPostsArePresent();
+    });
 
-  it('All posts contain expected content', () => {
-    homePage.assertAllPostsHaveBasicContent();
+    it("All posts contain expected content", () => {
+      homePage.assertAllPostsHaveExpectedContent();
+    });
+
+    it("Like and comment buttons are present in the first visible post", () => {
+      homePage.assertLikeAndCommentBtnsPresentInPost();
+    });
   });
 
-  it('Like and comment buttons are present in the first visible post', () => {
-    homePage.assertLikeAndCommentButtonsArePresentInFirstRealPost();
+  describe("Home Page - Like option", () => {
+    it("User can like the first visible post and like is visible", () => {
+      homePage.assertUserCanLikePost();
+    });
 
+    it("User can unlike the first visible post and like is not visible anymore", () => {
+      homePage.assertUserCanUnlikePost();
+    });
   });
 
-  it('User can like the first visible post and like is visible', () => {
-    homePage.assertUserCanLikeFirstRealPost();
+  describe("Home Page - Comment option", () => {
+    it("Comment modal window is displayed", () => {
+      homePage.clickCommentBtn();
+      homePage.assertCommentModalWndwVisible();
+    });
+
+    it("User can comment post and entered comment is saved", () => {
+      homePage.assertUserCanCommentPost();
+    });
+
+    it("User can delete the comment", () => {
+      homePage.clickCommentBtn();
+      homePage.assertUserCanDeleteComment();
+    });
+
+    it("User can close the Comment modal window ", () => {
+      homePage.clickCommentBtn();
+      homePage.assertUserCanCloseCommentModal();
+    });
   });
-
-  it('User can unlike the first visible post and like is not visible anymore', () => {
-    homePage.assertUserCanUnlikeFirstRealPost(); 
-  });
-
-  it('Comment modal window is opened when user click on the comment button on the first visible post', () => {
-    homePage.clickCommentOnFirstRealPost();
-    homePage.assertCommentsModalIsVisible();
-  });
-
-
 });
--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -1,11 +1,13 @@
 class HomePage {
 
+  commentText = 'New comment text added here';
+
   getAllPosts() {
-    return cy.get('.home__main__feed__post.card').not(':first');//skipping first post
+    return cy.get('.home__main__feed__post.card').not(':first'); //first post is skipped
   }
 
   getPostImage(post) {
-    return post.find('img#author-avatar'); // jer je jedini img unutar posta i uvek se nalazi prvi
+    return post.find('img#author-avatar');
   }
 
   getPostUserName(post) {
@@ -26,30 +28,60 @@ class HomePage {
     this.getAllPosts().should('have.length.at.least', 1);
   }
 
-  getLikeButton(post) {
+  getLikeBtn(post) {
     return post.find('button[aria-label="Post action button"]').has('svg[data-icon="heart"]');
   }
 
-  getCommentButton(post) {
+  getCommentBtn(post) {
     return post.find('button[aria-label="Post action button"]').has('svg[data-icon="comment"]');
   }
 
-  getCommentsModalHeader() {
+  getCommentsModalWndw() {
     return cy.get('.commentsModal__header.modal-header');
   }
 
+  getCommentInputField() {
+    return cy.get('input[placeholder="Write a comment"]');
+  }
+
+  getCommentSubmitBtn() {
+    return cy.get('button#createInputSubmitBtn');
+  }
+
+  getNewCommentPost() {
+    return cy.get('.post__comments');
+  }
+
+  getNewestCommentTxt() {
+    return cy.get('.post__description.post__description--modal').last();
+  }
+  getCommentDeleteBtn(commentText) {
+    return cy
+      .get('.post__comments__list__comment') // take all comments
+      .contains('.post__description--modal', commentText) // finding the one with the value from commentText
+      .parents('.post__comments__list__comment') // making sure that we are on the parent 
+      .find('button.post__deleteBtn') // find the button
+      .first(); // fallback, making sure that first delete button is clicked
+  }
+
+  getCloseModalBtn() {
+    return cy.get('.commentsModal__header.modal-header button.btn-close[aria-label="Close"]');
+  }
+
+
+
   clickLikeOnFirstRealPost() {
     this.getAllPosts().eq(0).then(($post) => {
-      const likeBtn = this.getLikeButton($post);
+      const likeBtn = this.getLikeBtn($post);
       cy.wrap(likeBtn)
         .scrollIntoView()
         .click();
     });
   }
 
-  clickCommentOnFirstRealPost() {
+  clickCommentBtn() {
     this.getAllPosts().eq(0).then(($post) => {
-      const commentBtn = this.getCommentButton($post);
+      const commentBtn = this.getCommentBtn($post);
       cy.wrap(commentBtn)
         .scrollIntoView()
         .click();
@@ -57,7 +89,9 @@ class HomePage {
 
   }
 
-  assertAllPostsHaveBasicContent() {
+
+
+  assertAllPostsHaveExpectedContent() {
     this.getAllPosts().each(($post) => {
       cy.wrap($post).within(() => {
         cy.wrap(this.getPostImage($post))
@@ -79,10 +113,10 @@ class HomePage {
     });
   }
 
-  assertLikeAndCommentButtonsArePresentInFirstRealPost() {
+  assertLikeAndCommentBtnsPresentInPost() {
     this.getAllPosts().eq(0).then(($post) => {
-      const likeBtn = this.getLikeButton($post);
-      const commentBtn = this.getCommentButton($post);
+      const likeBtn = this.getLikeBtn($post);
+      const commentBtn = this.getCommentBtn($post);
       cy.wrap(likeBtn)
         .scrollIntoView()
         .should('exist')
@@ -95,9 +129,9 @@ class HomePage {
     });
   }
 
-  assertUserCanLikeFirstRealPost() {
+  assertUserCanLikePost() {
     this.getAllPosts().eq(0).then(($post) => {
-      const likeBtn = this.getLikeButton($post);
+      const likeBtn = this.getLikeBtn($post);
 
       cy.wrap(likeBtn)
         .scrollIntoView()
@@ -105,14 +139,15 @@ class HomePage {
 
       cy.wrap(likeBtn).click();
 
+
       cy.wrap(likeBtn)
         .should('have.class', 'btn-primary');
     });
   }
 
-    assertUserCanUnlikeFirstRealPost() {
+  assertUserCanUnlikePost() {
     this.getAllPosts().eq(0).then(($post) => {
-      const likeBtn = this.getLikeButton($post);
+      const likeBtn = this.getLikeBtn($post);
 
       cy.wrap(likeBtn)
         .scrollIntoView()
@@ -126,10 +161,51 @@ class HomePage {
   }
 
 
-  assertCommentsModalHeaderIsVisible() {
-    this.getCommentsModalHeader()
+  assertCommentModalWndwVisible() {
+    this.getCommentsModalWndw()
       .should('exist')
       .and('be.visible');
+  }
+
+  assertUserCanCommentPost() {
+    this.clickCommentBtn();
+    this.getCommentInputField()
+      .should('exist')
+      .and('be.visible')
+      .type(this.commentText);
+
+    this.getCommentSubmitBtn()
+      .should('exist')
+      .and('be.visible')
+      .click();
+
+    this.getNewCommentPost()
+      .should('exist')
+      .and('be.visible')
+
+    this.getNewestCommentTxt()
+      .should('be.visible')
+      .and('have.text', this.commentText);
+
+  }
+  assertUserCanDeleteComment() {
+    this.getCommentDeleteBtn(this.commentText)
+      .should('be.visible')
+      .click();
+
+
+    cy.contains('.post__description.post__description--modal', this.commentText)
+      .should('not.exist');
+  }
+
+  assertUserCanCloseCommentModal() {
+    this.getCommentsModalWndw().should('exist').and('be.visible');
+
+    this.getCloseModalBtn()
+      .should('be.visible')
+      .click();
+
+    this.getCommentsModalWndw().should('not.exist');
   }
 
 
