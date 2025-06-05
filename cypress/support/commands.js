9f984a2bedcab76d010d9e0eb66a74d6481dbeb8
Ivana Radosevic Miricki <ivana_radosevicmiricki@epam.com>
2025-06-05 10:25:10 +0200

    Cleaning up the files

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,70 +1,28 @@
-Cypress.Commands.add('loginViaAPI', () => {
-  const loginUrl = Cypress.env('loginRequestLink');
-  const bearer = Cypress.env('bearerToken');
+Cypress.Commands.add("loginViaAPI", () => {
+  const loginUrl = Cypress.env("loginRequestLink");
+  const bearer = Cypress.env("bearerToken");
 
   cy.request({
-    method: 'POST',
+    method: "POST",
     url: loginUrl,
     headers: {
       Authorization: `Bearer ${bearer}`,
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: {
-      email: Cypress.env('email'),
-      password: Cypress.env('password')
-    }
+      email: Cypress.env("email"),
+      password: Cypress.env("password"),
+    },
   }).then((response) => {
     expect(response.status).to.eq(200);
-    console.log('Response body:', JSON.stringify(response.body));
+    console.log("Response body:", JSON.stringify(response.body));
 
     const token = response.body.token;
     if (!token) {
-      throw new Error('Token not found in response body');
+      throw new Error("Token not found in response body");
     }
 
-    cy.setCookie('token', token);
-    console.log('Received token:', token);
-
-
-
-    // // Ako backend automatski NE postavlja cookie, dodaj ručno:
-    // cy.setCookie('token', response.body.token);
-    // cy.log('Received token:', response.body.token);
-
-    // // Ako backend već postavi cookie sam (set-cookie), možda nije ni potrebno dodatno ručno
+    cy.setCookie("token", token);
+    console.log("Received token:", token);
   });
 });
-
-
-
-
-
-
-
-
-
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
